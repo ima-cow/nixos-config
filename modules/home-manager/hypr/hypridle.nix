fb039552535b37b04b77ee5923d3f07e6a886dc4
ima-cow <ethanthequag@gmail.com>
2025-10-08 12:17:42 -0400

    Update hypridle.nix

--- modules/home-manager/hypr/hypridle.nix
+++ modules/home-manager/hypr/hypridle.nix
@@ -1,12 +1,33 @@
 { lib, config, pkgs, ...}:
 
 {
-  options.hypridle.enable
+  options.hypridle = enable
     = lib.mkEnableOption "enable user module";
 
   config = lib.mkIf config.hypridle.enable {
     programs.hypridle = {
       enable = true;
+
+      listener = [
+        {
+          timeout = 150;
+          on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
+          on-resume = "brightnessctl -rd rgb:kbd_backlight";
+        }
+        {
+          timeout = 300;
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 330;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on && brightnessctl -r";
+        }
+        {
+          timeout = 1800;
+          on-timeout = "systemctl suspend";
+        }
+      ];
     };
   };
 }
