7d6689c89449095e6335574d8a6d9eee6d1499e4
ima-cow <ethanthequag@gmail.com>
2025-10-08 14:59:46 -0400

    iodk

--- modules/home-manager/modules.nix
+++ modules/home-manager/modules.nix
@@ -9,10 +9,7 @@
       ./hypr/hyprland.nix
       ./hypr/waybar.nix
       ./hypr/hyprlock.nix
-<<<<<<< HEAD
-=======
       ./hypr/hypridle.nix
->>>>>>> 5208547cb0c1e4378f63462f96bde0988e7ff474
     ];
 }
 
